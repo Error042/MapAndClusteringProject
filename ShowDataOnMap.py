e6da88a07cf65cef604bfadbc5ba7cbd74e5c629
Error042 <58344240+Error0341@users.noreply.github.com>
2021-03-17 14:55:59 +0100

    Changed how the map behaves. Now it is square and the code is more divided in sections.

--- ShowDataOnMap.py
+++ ShowDataOnMap.py
@@ -16,12 +16,16 @@ with open('TestRoute.csv', 'r') as file:
         ys.append(float(data[1]))
         xs.append(float(data[2]))
 
-# creating the CS with the specifications
-plt.plot(xs, ys, 'r-o')
+# creating the CS with the map.png in the background
+imData = plt.imread('map.png')
+fig, ax = plt.subplots()
+ax.imshow(imData, extent=[12.25800, 12.47678, 51.27130, 51.40799])
+ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
+
+# placing the coordinates in the CS and labeling the axes
+plt.plot(xs, ys, color='red', marker='o')
 plt.xlabel('Latitude')
 plt.ylabel('Longitude')
-imData = plt.imread('map.png')
-plt.imshow(imData, extent=[12.32117, 12.41747, 51.30025, 51.36042])
 
-# showing the final result
+# show the final result
 plt.show()
